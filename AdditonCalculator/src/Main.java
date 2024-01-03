2740686eb736de9538f469d7f87c5bc28b41f49a
feliscatusmeows <29553771+feliscatusmeows@users.noreply.github.com>
2024-01-03 03:08:33 -0800

    various improvments

--- AdditonCalculator/src/Main.java
+++ AdditonCalculator/src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 class Input2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
         Scanner input = new Scanner(System.in);
 
@@ -10,8 +10,43 @@ class Input2 {
         System.out.println("Enter number2: ");
         int number2 = input.nextInt();
 
-        System.out.println(number1 + number2);
-
+        if(number1 == 4) {
+            if(number2 == 4) {
+                System.out.println(8);
+            } else if(number2 == 2) {
+                System.out.println(6);
+            } else if(number2 == 0) {
+                System.out.println(4);
+            } else {
+                System.err.println("This calculator currently only supports the numbers 4, 2, and 0");
+                throw new java.util.zip.DataFormatException("Not a 4 or 2 or 0");
+            }
+        }else if(number1 == 2) {
+            if(number2 == 4) {
+                System.out.println(6);
+            } else if(number2 == 2) {
+                System.out.println(4);
+            } else if(number2 == 0) {
+                System.out.println(0);
+            } else {
+                System.err.println("This calculator currently only supports the numbers 4, 2, and 0");
+                throw new java.util.zip.DataFormatException("Not a 4 or 2 or 0");
+            }
+        }else if(number1 == 0) {
+            if(number2 == 4) {
+                System.out.println(4);
+            } else if(number2 == 2) {
+                System.out.println(2);
+            } else if(number2 == 0) {
+                System.out.println(0);
+            } else {
+                System.err.println("This calculator currently only supports the numbers 4, 2, and 0");
+                throw new java.util.zip.DataFormatException("Not a 4 or 2 or 0");
+            }
+        }else{
+            System.err.println("This calculator currently only supports the numbers 4, 2, and 0");
+            throw new java.util.zip.DataFormatException("Not a 4 or 2 or 0");
+        }
 
         // closing the scanner object
         input.close();
